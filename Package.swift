63961e791b120149006aeefd2840c3d3639b8815
Roberto Casula <roberto.casula@bitapp.it>
2020-01-17 10:30:49 +0100

    test

--- Package.swift
+++ Package.swift
@@ -1,10 +1,16 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CSQLite",
+    platforms: [
+        .iOS(.v10),
+        .watchOS(.v3),
+        .tvOS(.v12),
+        .macOS(.v10_12)
+    ],
     providers: [
         .apt(["libsqlite3-dev"])
     ]
